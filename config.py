6cf7373d0fdb53abc7af70d0c2552feeba860bc7
User <dossovmarlen85@gmail.com>
2022-12-12 15:04:23 +0300

    I cannot start app

--- config.py
+++ config.py
@@ -4,5 +4,5 @@
 class Config:
     DEBUG = False
     JSON_AS_ASCII = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///movies.db' #:memory:'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
